bd411f28143d6103a1c60e7b52f73dac75ba2244
Adrian Karani <karanimuturi@gmail.com>
2019-10-03 13:11:19 +0300

    Initial commit

---
+++ src/main/java/com/example/springgrp5csproject/SpringGrp5csProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgrp5csproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGrp5csProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGrp5csProjectApplication.class, args);
+    }
+
+}
